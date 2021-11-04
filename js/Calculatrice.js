cafe24fe352d87422c0165e1044468b85f1d8e7b
clavainova <clavainova@gmail.com>
2021-11-04 15:20:15 +0100

    trying alternative api

---
+++ js/Calculatrice.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    ajouter: function (a, b) { return a + b; },
+    diviser: function (a, b) { return a / b; },
+    multipliquer: function (a, b) { return a * b; },
+    soustraire: function (a, b) { return a - b; },
+    wait: function (time, callback) {
+        setTimeout(function () {
+            callback(56);
+        }, time)
+    }
+}
--- test/index.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require("assert");
-const addFn = require("../index.js");
-
-describe("TestAdd", function () {
-    it("should return 10", function () {
-        assert.equal(addFn.add(4, 6), 10);
-    });
-})
