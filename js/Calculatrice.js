b2583c73f8e563a85ca635e789c5fee7ef67a5fd
clavainova <clavainova@gmail.com>
2021-11-04 15:33:55 +0100

    alt api fix

--- js/Calculatrice.js
+++ js/Calculatrice.js
@@ -4,9 +4,9 @@ module.exports = {
     diviser: function (a, b) { return a / b; },
     multipliquer: function (a, b) { return a * b; },
     soustraire: function (a, b) { return a - b; },
-    wait: function (time, callback) {
-        setTimeout(function () {
-            callback(56);
-        }, time)
-    }
+    // wait: function (time, callback) {
+    //     setTimeout(function () {
+    //         callback(56);
+    //     }, time)
+    // }
 }
